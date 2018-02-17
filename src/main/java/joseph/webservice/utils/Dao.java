d0b8750265214e8bba6ad6f0718df526fce4485d
Joseph <joseph.a.gauthier@gmail.com>
2018-02-16 20:15:36 -0600

    Forgot one :P

--- src/main/java/joseph/webservice/utils/Dao.java
+++ src/main/java/joseph/webservice/utils/Dao.java
@@ -1,29 +1,48 @@
-package joseph.webservice.classes;
+package joseph.webservice.utils;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import joseph.webservice.pojos.LoginPacket;
 
 /* 	
+ 	To view database in a browser:
+ 		Navigate to phpmyadmin.co
+ 		Use below login credentials to log in
+ 	
 	Database Connection Details:
-		Server: sql9.freemysqlhosting.net
-		Name: sql9195935
-		Username: sql9195935
-		Password: lp5FPtqSb4
+		Server: sql3.freesqldatabase.com
+		Name: sql3205145
+		Username: sql3205145
+		Password: rDlDd1QCsz
 		Port number: 3306
  */
 
 public class Dao {
+	
+	private static final Logger log = Logger.getLogger( Dao.class.getName() );
 	Connection conn;
+	
+	String user = "sql3205145";
+	String pass = "rDlDd1QCsz";
+	String address = "jdbc:mysql://sql3.freesqldatabase.com:3306/sql3205145";
+	
 	public Dao()
 	{
 		conn = null;
 		try{
+			log.info("Connecting to " + address);
 			Class.forName("com.mysql.jdbc.Driver");
 			Properties properties = new Properties();
-			properties.setProperty("user", "sql9195935");
-			properties.setProperty("password", "lp5FPtqSb4");
+			properties.setProperty("user", user);
+			properties.setProperty("password", pass);
 			properties.setProperty("useSSL", "false");
-			conn = DriverManager.getConnection("jdbc:mysql://sql9.freemysqlhosting.net:3306/sql9195935", properties);
+			conn = DriverManager.getConnection(address, properties);
+			log.info("Connected!");
 		}
 		catch(Exception e)
 		{
@@ -31,84 +50,94 @@ public class Dao {
 		}
 	}
 
-	public DataPacket getAccount(String username)
+	public LoginPacket getAccount(String username)
 	{
 		try {			
+			log.info("Getting accounts");
 			Statement stmt = conn.createStatement() ;
-			String query = "select * from Users where User = '" + username + "';" ;
+			String query = "select * from UserAccounts where user = '" + username + "';" ;
+			log.info("Query: " + query);
 			ResultSet rs = stmt.executeQuery(query) ;
-			
-			int resultSetSize = 0;
-			try {
-			    rs.last();
-			    resultSetSize = rs.getRow();
-			    rs.beforeFirst();
-			}
-			catch(Exception e) {
-				e.printStackTrace();
-			}
-			
-			DataPacket[] packets = new DataPacket[resultSetSize];
-			int counter = 0;
-			while(rs.next())
-			{
-				packets[counter] = new DataPacket(rs.getString("User"), rs.getString("Pass"));
-				counter++;
-			}
 			conn.close();
-			return packets[0];
+			if(rs.getFetchSize() > 1) {
+				String response = "More than 1 account for this user";
+				log.log(Level.SEVERE, response);
+				return new LoginPacket(response, "lol");
+			}
+			else if (rs.getFetchSize() == 0) {
+				String response = "No account for this user";
+				log.log(Level.SEVERE, response);
+				return new LoginPacket(response, "lol");
+			}
+			else {
+				LoginPacket response = new LoginPacket(rs.getString("user"), rs.getString("pass"));
+				log.info("Returning: " + response.toString());
+				return response;
+			}
 		} 
 		catch (Exception e) {
 			e.printStackTrace();
-			DataPacket errorPacket = new DataPacket("error...", "1776-07-04");
+			LoginPacket errorPacket = new LoginPacket(e.getMessage(), e.getStackTrace().toString());
+			e.printStackTrace();
+			log.log(Level.SEVERE, e.getMessage());
 			return errorPacket;
 		}
 	}
 	
-	public DataPacket[] getAccounts()
+	public List<LoginPacket> getAccounts()
 	{
 		try {			
+			log.info("Getting all accounts");
 			Statement stmt = conn.createStatement() ;
-			String query = "select * from Users;" ;
+			String query = "select * from UserAccounts;" ;
 			ResultSet rs = stmt.executeQuery(query) ;
-			
-			int resultSetSize = 0;
-			try {
-			    rs.last();
-			    resultSetSize = rs.getRow();
-			    rs.beforeFirst();
-			}
-			catch(Exception e) {
-				e.printStackTrace();
-			}
-			
-			DataPacket[] packets = new DataPacket[resultSetSize];
-			int counter = 0;
-			while(rs.next())
-			{
-				packets[counter] = new DataPacket(rs.getString("User"), rs.getString("Pass"));
-				counter++;
-			}
+			List<LoginPacket> responseList = new ArrayList<LoginPacket>();
 			conn.close();
-			return packets;
+			
+			if(rs.getFetchSize() > 1) {
+				String response = "More than 1 account for this user";
+				log.log(Level.SEVERE, response);
+				responseList.add(new LoginPacket(response, "lol"));
+				return responseList;
+			}
+			else if (rs.getFetchSize() == 0) {
+				String response = "No account for this user";
+				log.log(Level.SEVERE, response);
+				responseList.add(new LoginPacket(response, "lol"));
+				return responseList;
+			}
+			else {
+				LoginPacket response = new LoginPacket(rs.getString("user"), rs.getString("pass"));
+				log.info("Returning: " + response.toString());
+				responseList.add(response);
+				return responseList;
+			}
 		} 
 		catch (Exception e) {
 			e.printStackTrace();
-			DataPacket errorPackets[] = {new DataPacket("error...", "1776-07-04")};
-			return errorPackets;
+			LoginPacket errorPacket = new LoginPacket(e.getMessage(), e.getStackTrace().toString());
+			e.printStackTrace();
+			log.log(Level.SEVERE, e.getMessage());
+			List<LoginPacket> responseList = new ArrayList<LoginPacket>();
+			responseList.add(errorPacket);
+			return responseList;
 		}
 	}
 	
 	public int addAccount(String username, String password)
 	{
 		try {			
+			log.info("Adding user: " + username + ", " + password);
 			Statement stmt = conn.createStatement() ;
-			String query = "insert into Users values('" + username + "', '" + password + "');" ;
+			String query = "insert into users values('" + username + "', '" + password + "');" ;
+			log.info("Query: " + query);
 			int numRowsAffected = stmt.executeUpdate(query) ;
+			log.info("Added " + numRowsAffected + " users");
 			return numRowsAffected;
 		} 
 		catch (Exception e) {
 			e.printStackTrace();
+			log.log(Level.SEVERE, e.getMessage());
 			return 0;
 		}
 	}
